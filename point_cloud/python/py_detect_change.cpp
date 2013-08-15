b8a0c3d5133e13b1d2a2d628e8478077b4c615bd
Peter Morton <petermorton@gmail.com>
2013-08-15 18:56:29 +1000

    Switched to boost python bindings

--- point_cloud/python/py_detect_change.cpp
+++ point_cloud/python/py_detect_change.cpp
@@ -1,87 +1,36 @@
-// This file is part of snark, a generic and flexible library for robotics research
-// Copyright (c) 2011 The University of Sydney
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. All advertising materials mentioning features or use of this software
-//    must display the following acknowledgement:
-//    This product includes software developed by the The University of Sydney.
-// 4. Neither the name of the The University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-/// @author peter morton
-
-#include <Python.h>
-#define PY_ARRAY_UNIQUE_SYMBOL _snark_point_cloud_ARRAY_API
-#define NO_IMPORT_ARRAY
-#include <numpy/arrayobject.h>
-#include <snark/point_cloud/detect_change.h>
 #include <snark/point_cloud/python/py_detect_change.h>
+#include <vector>
 
 
-// This function reimplements the functionality of points-detect-change
-PyObject* detect_change_cfunc(PyObject *dummy, PyObject *args, PyObject *kwds)
+ChangeDetector::ChangeDetector(const bn::ndarray& ref, double angle_threshold,
+                               double range_threshold)
+    : grid_(grid_t::point_type(angle_threshold, angle_threshold)),
+      angle_threshold_(angle_threshold), range_threshold_(range_threshold)
 {
-    PyObject *arg1=NULL, *arg2=NULL;
-    PyObject *arr1=NULL, *arr2=NULL;
+    // Check that input is as expected.
+    if (ref.get_dtype() != bn::dtype::get_builtin<double>()) {
+        PyErr_SetString(PyExc_TypeError, "Incorrect array data type (expected double)");
+        bp::throw_error_already_set();
+    }
+    if(ref.get_nd() != 2 || ref.shape(1) != 3) {
+        PyErr_SetString(PyExc_ValueError, "Incorrect dimensions (expected N x 3)");
+        bp::throw_error_already_set();
+    }
 
-    // Default values.
-    double angle_threshold = 0.0157; // 0.9 degrees.
-    double range_threshold = 0.15;
+    // Compute grid.
+    const int n_points = ref.shape(0);
 
-    static char *kwlist[] = {
-        "reference",
-        "scan",
-        "angle_threshold",
-        "range_threshold",
-        NULL
-    };
+    int row_stride = ref.strides(0) / sizeof(double);
+    int col_stride = ref.strides(1) / sizeof(double);
 
-    // Parse input arrays and keyword parameters.
-    if (!PyArg_ParseTupleAndKeywords(args, kwds, "O!O!|dd", kwlist,
-                &PyArray_Type, &arg1, &PyArray_Type, &arg2,
-                &angle_threshold, &range_threshold)) return NULL;
-
-    // Convert inputs into numpy arrays.
-    arr1 = PyArray_FROM_OTF(arg1, NPY_DOUBLE, NPY_IN_ARRAY);
-    if (arr1 == NULL) return NULL;
-    arr2 = PyArray_FROM_OTF(arg2, NPY_DOUBLE, NPY_IN_ARRAY);
-    if (arr2 == NULL) { Py_XDECREF(arr1); return NULL; }
-
-    // Define the voxel grid.
-    typedef snark::voxel_map<cell, 2 > grid_t;
-    snark::voxel_map< int, 2 >::point_type resolution;
-    resolution = grid_t::point_type( angle_threshold, angle_threshold );
-    grid_t grid( resolution );
-
-    // Load reference scan.
-    comma::uint64 index = 0;
-    for (int pt_idx = 0; pt_idx < PyArray_DIM(arr1, 0); pt_idx++) {
-        point_t p( *(double *)PyArray_GETPTR2(arr1, pt_idx, 0),
-                   *(double *)PyArray_GETPTR2(arr1, pt_idx, 1),
-                   *(double *)PyArray_GETPTR2(arr1, pt_idx, 2));
-        cell::entry entry(p, index);
+    double *row_iter = reinterpret_cast<double *>(ref.get_data());
+    for (int pt_idx = 0; pt_idx < n_points; ++pt_idx, row_iter += row_stride) {
+        double *col_iter = row_iter;
+        point_t p(*col_iter, 
+                  *(col_iter + col_stride), 
+                  *(col_iter + 2 * col_stride));
+        
+        cell::entry entry(p, pt_idx);
         for( int i = -1; i < 2; ++i )
         {
             for( int j = -1; j < 2; ++j )
@@ -90,35 +39,59 @@ PyObject* detect_change_cfunc(PyObject *dummy, PyObject *args, PyObject *kwds)
                 if( bearing < -M_PI ) { bearing += ( M_PI * 2 ); }
                 else if( bearing >= M_PI ) { bearing -= ( M_PI * 2 ); }
                 double elevation = p.elevation() + angle_threshold * j;
-                grid_t::iterator it = grid.touch_at( grid_t::point_type( bearing, elevation ) );
-                it->second.add( entry ); //it->second.add_to_grid( entry );
+                grid_t::iterator it = grid_.touch_at( grid_t::point_type( bearing, elevation ) );
+                it->second.add( entry );
             }
         }
     }
-
-    // Create output array.
-    npy_intp dims[1];
-    dims[0] = PyArray_DIM(arr2, 0);
-    PyObject* oarr = PyArray_SimpleNew(1, dims, NPY_BOOL);
-
-    // Run change detection.
-    for (int pt_idx = 0; pt_idx < PyArray_DIM(arr2, 0); pt_idx++) {
-        bool change = true;
-        point_t p( *(double *)PyArray_GETPTR2(arr2, pt_idx, 0),
-                   *(double *)PyArray_GETPTR2(arr2, pt_idx, 1),
-                   *(double *)PyArray_GETPTR2(arr2, pt_idx, 2));
-        grid_t::const_iterator it = grid.find( grid_t::point_type( p.bearing(), p.elevation() ) );
-        if( it == grid.end() ) { 
-            change = false; 
-        } else {
-            const cell::entry* q = it->second.trace( p, angle_threshold, range_threshold );
-            if( !q ) { change = false; }
-        }
-        *(bool *)PyArray_GETPTR1(oarr, pt_idx) = change;
-    }
-
-    Py_DECREF(arr1);
-    Py_DECREF(arr2);
-    return oarr;
 }
 
+bn::ndarray ChangeDetector::GetChanges(const bn::ndarray& scan) 
+{
+    // Check that input is as expected.
+    if (scan.get_dtype() != bn::dtype::get_builtin<double>()) {
+        PyErr_SetString(PyExc_TypeError, "Incorrect array data type (expected double)");
+        bp::throw_error_already_set();
+    }
+    if(scan.get_nd() != 2 || scan.shape(1) != 3) {
+        PyErr_SetString(PyExc_ValueError, "Incorrect dimensions (expected N x 3)");
+        bp::throw_error_already_set();
+    }
+
+    // Create result array.
+    const int n_points = scan.shape(0);
+    bn::dtype dt = bn::dtype::get_builtin<bool>();
+    bp::tuple shape = bp::make_tuple(n_points);
+    bn::ndarray result = bn::zeros(shape, dt);
+    uint8_t* changes = reinterpret_cast<uint8_t *>(result.get_data());
+
+    // Detect changes.
+    int row_stride = scan.strides(0) / sizeof(double);
+    int col_stride = scan.strides(1) / sizeof(double);
+    double *row_iter = reinterpret_cast<double *>(scan.get_data());
+    for (int pt_idx = 0; pt_idx < n_points; ++pt_idx, row_iter += row_stride) {
+        bool change = true;
+        double *col_iter = row_iter;
+        point_t p(*col_iter, 
+                  *(col_iter + col_stride), 
+                  *(col_iter + 2 * col_stride));
+
+        grid_t::const_iterator it = grid_.find( grid_t::point_type( p.bearing(), p.elevation() ) );
+        if( it == grid_.end() ) { 
+            change = false; 
+        } else {
+            const cell::entry* q = it->second.trace( p, angle_threshold_, range_threshold_ );
+            if( !q ) { change = false; }
+        }
+        changes[pt_idx] = change;
+    }
+
+    return result;
+}
+
+bn::ndarray detect_change(const bn::ndarray& ref, const bn::ndarray& scan,
+                          double angle_threshold, double range_threshold) 
+{
+    ChangeDetector cd(ref, angle_threshold, range_threshold);
+    return cd.GetChanges(scan);
+}
--- point_cloud/python/py_detect_change.h
+++ point_cloud/python/py_detect_change.h
@@ -1,37 +1,25 @@
-// This file is part of snark, a generic and flexible library for robotics research
-// Copyright (c) 2011 The University of Sydney
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// 1. Redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-// 2. Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-// 3. All advertising materials mentioning features or use of this software
-//    must display the following acknowledgement:
-//    This product includes software developed by the The University of Sydney.
-// 4. Neither the name of the The University of Sydney nor the
-//    names of its contributors may be used to endorse or promote products
-//    derived from this software without specific prior written permission.
-//
-// NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-// GRANTED BY THIS LICENSE.  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-// HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-/// @author peter morton
+#include <boost/python.hpp>
+#include <boost/numpy.hpp>
+#include <snark/point_cloud/detect_change.h>
 
-#include <Python.h>
+namespace bp = boost::python;
+namespace bn = boost::numpy;
 
-PyObject* detect_change_cfunc(PyObject *dummy, PyObject *args, PyObject *kwds);
+typedef snark::voxel_map<cell, 2 > grid_t;
+
+class ChangeDetector
+{
+public:
+    ChangeDetector(const bn::ndarray& ref, double angle_threshold, 
+                   double range_threshold);
+
+    bn::ndarray GetChanges(const bn::ndarray& scan);
+
+private:
+    grid_t grid_;
+    double angle_threshold_;
+    double range_threshold_;
+};
+
+bn::ndarray detect_change(const bn::ndarray& ref, const bn::ndarray& scan,
+                          double angle_threshold, double range_threshold);
